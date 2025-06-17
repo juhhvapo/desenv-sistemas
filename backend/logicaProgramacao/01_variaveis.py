12490f9096a0965c5845cf9b6c88aaf9c2fec16a
Julia souza <jsm581160@gmail.com>
2025-06-17 09:53:48 -0300

    Trabalhando com variaveis

---
+++ backend/logicaProgramacao/01_variaveis.py
@@ -0,0 +1,33 @@
+nome = "Julia"
+idade = 17
+altura = 1.61
+tem_carteira = False
+
+print("Nome:", nome)
+print("Idade:", idade)
+print("Altura:", altura)
+print("Tem carteira?", tem_carteira)
+
+animal = "Macaco"
+comida = "Arroz"
+
+print("Animal:", animal)
+print("Comida:", comida)
+
+numero = 10
+ano = 2025
+
+print("Numero:", numero)
+print("Ano:", ano)
+
+peso = 51,20
+temperatura = 20,7
+
+print("Peso:", peso)
+print("Temperatura:", temperatura)
+
+tem_sede = True
+tem_cabelo = False
+
+print("Tem sede?", tem_sede)
+print("Tem cabelo?", tem_cabelo) 
