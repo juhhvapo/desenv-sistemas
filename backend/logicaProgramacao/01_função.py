aa2dd8c241873d2af6b80841a761c10115c0fdc4
júlia <jsm581160@gmail.com>
2025-08-05 09:39:33 -0300

    exercicio função

---
+++ backend/logicaProgramacao/01_função.py
@@ -0,0 +1,10 @@
+def saudacao (nome):
+   print("Olá", nome)
+
+saudacao("Júlia")
+
+def soma(num1,num2):
+   soma = num1 + num2
+   print("soma:", soma)
+
+soma(3,4)
--- backend/logicaProgramacao/exercicio_laço04.py
+++ backend/logicaProgramacao/exercicio_laço04.py
@@ -1,5 +1,17 @@
-Pedir nome e senha do usuário
-mostrar "bem vindo" quando acertar a senha e o nome 
-após,pedir o salário do usuario
-mostrar salario anual
-se o salário anual for maior que 100 mil mostrar mensagem rico
+senhacorreta = input("Digite a senha correta:")
+senha = input("Digite a senha:")
+nome = input("Digite o seu nome:")
+salario = float (input("Digite seu salario:"))
+
+while senha!= senhacorreta: 
+    print("senha incorreta")
+    senha= input("Digite a senha novamente:")
+
+    print("Bem-vindo", nome)
+
+    salarioanual= salario * 12 
+
+if(salarioanual > 1000000):
+    print("rico")
+else:
+    print("Faz o L")
