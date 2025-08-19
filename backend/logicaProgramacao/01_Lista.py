001fed1546d16db8964b2c6f810ba7913d66008d
júlia <jsm581160@gmail.com>
2025-08-19 09:41:01 -0300

    exercicios lista

---
+++ backend/logicaProgramacao/01_Lista.py
@@ -0,0 +1,16 @@
+#Crie uma lista de tarefas
+#Adicione 5 tarefas na lista
+#Adicione uma tarefa na posição 2 da lista
+#Remova a tarefa "lavar louça" da lista
+#Remover a tarefa da posição 1 da lista
+
+tarefas = [
+    "Lavar a louça"
+    "Levar o cachorro pra passear"
+    "Estudar Geografia"
+    "Lavar o carro"
+    "Fazer compras"
+]
+tarefas.insert(2,"Lavar a roupa")
+tarefas.remove("Lavar a louça")
+
---
+++ backend/logicaProgramacao/02_Lista.py
@@ -0,0 +1,21 @@
+#1: Crie uma lista de 10 alunos ; mostre "Olá" + nome de cada aluno
+#2: Crie uma lista de 10 números ; mostre a soma de todos os números da lista
+#3: Crie uma lista de 20 números ; diga quantos números pares e ímpares tem na lista
+
+#1
+alunos = ["Júlia","Clara","Maria","Daniela","Fabricio","Laura","Maria","Geovana","Beatriz","Guilherme"]
+
+for aluno in alunos:
+    print("Olá"+ aluno)
+
+
+#2
+numeros = []
+numeros.append(1,2,3,4,5,6,7,8,9,10)
+
+for numero in numeros:
+
+    soma += numeros
+
+
+#3
