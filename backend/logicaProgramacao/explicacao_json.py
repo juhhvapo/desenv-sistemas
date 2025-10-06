2d573a8f7a72c513f5aede48162c0974b2ba6d01
júlia <jsm581160@gmail.com>
2025-10-06 08:54:48 -0300

    modificando json

--- backend/logicaProgramacao/explicacao_json.py
+++ backend/logicaProgramacao/explicacao_json.py
@@ -16,16 +16,18 @@ except FileNotFoundError:
 
 
 try:
-    nome = input ("digite o nome do produto: ")
-    quantidade = int(input("digite a quantidade: ")) 
-    preco = float(input("digite o preço: "))
+    id = int(input("Digite o id do produto: "))
+    nome = input ("Digite o nome do produto: ")
+    quantidade = int(input("Digite a quantidade: ")) 
+    preco = float(input("Digite o preço: "))
 
 except ValueError:
     print("digite o valor corretamente")
 
 
 #Montar o objeto
-novo_produto = {"nome": nome,
+novo_produto = {"id": id,
+                "nome": nome,
                 "quantidade": quantidade,
                 "preco": preco,
                 "em_estoque": quantidade > 0 #expressão verdadeiro falso
@@ -39,3 +41,38 @@ with open ("loja.json", "w")as arquivo:
     #indent - formatar o arquivo json
 
     print("produto cadastrado com sucesso")
+
+
+#----- Modificar produto -----
+
+id_produto_modificar = int(input("Digite o id para modificar: "))
+nova_quantidade = int(input("Digite a nova quantidade: "))
+
+try:
+    with open("loja.json", "r") as arquivo:
+        inventario = json.load(arquivo)
+
+except FileNotFoundError:
+    print("Arquivo não encontrado")
+
+produto_encontrado = False
+
+for produto in inventario:
+    if produto['id'] == id_produto_modificar:
+    #colocamos a nova quantidade
+        produto['quantidade'] = nova_quantidade
+        produto['em_estoque'] = nova_quantidade > 0
+        produto_encontrado = True
+        break;
+
+if not produto_encontrado:
+    print("O produto com o id informado não foi encontrado")
+
+else:
+    with open("loja.json", "w")as arquivo:
+        json.dump(inventario, arquivo, indent=4)
+    print("o arquivo foi alterado com sucesso!!")
+    
+
+
+
